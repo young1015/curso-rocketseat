1e731ada22ded9e8f01588a06a7137a668bdcf8f
Robert Young <youngrobertbzs@gmail.com>
2023-10-15 02:08:23 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/robertyoung/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,74 @@
+package br.com.robertyoung.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.robertyoung.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+	
+	@Autowired
+	private IUserRepository userRepository;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		var servletPath = request.getServletPath();
+		
+		if(servletPath.startsWith("/tasks/")) {
+			
+			//Pegar a autenticação (usuário e senha)
+			
+			var authoritzation = request.getHeader("Authorization");
+			
+			var authEncoded = authoritzation.substring("Basic".length()).trim();
+			
+			byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+			
+			var authString = new String(authDecoded);
+			
+			// ["robertyoung", "12345"]
+			
+			String[] credentials = authString.split(":");
+			String username = credentials[0];
+			String password = credentials[1]; 
+					
+			// Validar usuário
+			
+			var user = this.userRepository.findByUsername(username);
+			if(user == null) {
+				response.sendError(401);			
+			} else {
+				var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+				if(passwordVerify.verified) {
+					request.setAttribute("idUser", user.getId());
+					filterChain.doFilter(request, response);
+				} else {
+					response.sendError(401);
+				}
+			}	
+		} else {
+			filterChain.doFilter(request, response);
+		}
+		
+
+		
+		
+		
+		
+	}
+
+	
+
+}
---
+++ src/main/java/br/com/robertyoung/todolist/task/ITaskRepository.java
@@ -0,0 +1,11 @@
+package br.com.robertyoung.todolist.task;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+	
+	List<TaskModel> findByIdUser(UUID idUser);
+}
